55e2c924485e0a161826abc3fd008487fd80c871
Alan e Edson <alanoliveiran45@gmail.com>
2017-02-23 20:56:01 -0300

    Add show route to delivery receipt

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :equipment, only: [:new, :create, :show]
   resources :proposals, only: [:new, :create, :show]
   resources :prices, only:[:new, :create, :show]
+  resources :delivery_receipts, only:[:show]
 end
