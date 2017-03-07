b4b2693f2c690c71ee14afb904628a666af99baa
Luiza e Anderson <luizalmagalhaes@hotmail.com>
2017-03-06 22:00:48 -0300

    create migration add rent to equipment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224000328) do
+ActiveRecord::Schema.define(version: 20170307005742) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170224000328) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "category_id"
+    t.boolean  "rent"
     t.index ["category_id"], name: "index_equipment_on_category_id"
   end
 
