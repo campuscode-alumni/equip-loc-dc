cbc71e73275421d12e61d5c15dc45d252f68cce8
Rafael e Henrique <henriquepjv@gmail.com>
2017-03-08 19:35:55 -0300

    editing euipment show

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -3,7 +3,7 @@ class Equipment < ApplicationRecord
   validates :serial_number, :name, :description, :acquisition_date, :replacement_value,
             :usage_limit, :image, :category, :manufacturer, :supplier, presence: true
 
-  validates :replacement_value, numericality: {greater_than: 0}
+#   validates :replacement_value, numericality: {greater_than: 0}
 
   belongs_to :category
 
